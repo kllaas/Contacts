58194eb5c87d13eb222c35e610598f9f438a0b7e
alexey <kll.a.s@yandex.ru>
2017-03-02 16:14:21 +0200

    color change, add cat image =)

--- app/src/main/java/com/example/alexey/contacts/activities/add_contact/CreateContactDialog.java
+++ app/src/main/java/com/example/alexey/contacts/activities/add_contact/CreateContactDialog.java
@@ -4,12 +4,10 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -29,14 +27,20 @@ public class CreateContactDialog extends DialogFragment implements CreateContact
 
     @BindView(R.id.et_first_name)
     EditText firstName;
+
     @BindView(R.id.et_second_name)
     EditText secondName;
+
     @BindView(R.id.et_phone)
     EditText phone;
+
     @BindView(R.id.et_email)
     EditText email;
+
     private CreateContactRelations.Presenter mPresenter;
+
     private ContactsRelations.OnCompleteListener mListener;
+
 
     public static CreateContactDialog newInstance() {
         return new CreateContactDialog();
@@ -62,8 +66,8 @@ public class CreateContactDialog extends DialogFragment implements CreateContact
         final AlertDialog dialog = builder.create();
 
         dialog.setOnShowListener(dialogInterface -> {
-            Button button = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
 
+            Button button = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
             button.setOnClickListener(view ->
 
                     mPresenter.tryToSave(firstName.getText().toString(),
@@ -76,13 +80,6 @@ public class CreateContactDialog extends DialogFragment implements CreateContact
         dialog.show();
 
         return dialog;
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        setCancelable(false);
-        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
